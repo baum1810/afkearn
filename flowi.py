aba630bdf2192b4fc95e7d64eb6b5d9c01fa532a
baum1810 <72774679+baum1810@users.noreply.github.com>
2023-02-15 20:27:16 +0100

    Update flowi.py

--- flowi.py
+++ flowi.py
@@ -4,7 +4,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 
 #add your cookie here
-cookie = "YOURCOOKIEHERE"
+cookie = "yourcookie"
 #domain of flow here they sometimes change the domain
 domain = "https://flownew.vercel.app/"
 
@@ -22,81 +22,83 @@ driver.get(domain)
 links = 0
 
 def main():
-    driver.get(domain)
-    global  links
-    system("cls")
-    print(f"Links: {links}")
-    time.sleep(2)
-    driver.execute_script('document.getElementById("generate").click()')
-    time.sleep(2)
     try:
-        driver.execute_script("document.querySelector('.btn.btn-primary.btn-block').click();")
-    except:
-        while "Too many requests, please try again later." in driver.page_source:
-            time.sleep(10)
-            driver.get(domain)
-    time.sleep(2)
-
-    if "https://bstlar.com" in driver.current_url:
-        for i in range(3):
-            try:
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[0])
-                driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/button').click()
-                link1 = driver.page_source
-                link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
-                link1 = link1[1].split('" target="_BLANK')
-                link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
-                driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[1])
-                driver.close()
-            except:pass
-        for i in range(3):
-            try:
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[0])
-                driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[2]/button').click()
-                link1 = driver.page_source
-                link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
-                link1 = link1[1].split('" target="_BLANK')
-                link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
-                driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[1])
-                driver.close()
-            except:pass
-        for i in range(3):
-            try:
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[0])
-                driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[3]/button').click()
-                link1 = driver.page_source
-                link1 = link1.split('https://searchwithme.net/redirect?tid=')
-                link1 = link1[1].split('" target="_BLANK')
-                link1 = f"https://searchwithme.net/redirect?tid={link1[0]}"
-                driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[1])
-                driver.close()
-            except:pass
-
-        tabs = driver.window_handles
-        driver.switch_to.window(tabs[0])
-        time.sleep(10)
-        while not "Unlock progress: 3 /" in driver.page_source:
-            time.sleep(0.1)
+        driver.get(domain)
+        global  links
+        system("cls")
+        print(f"Links: {links}")
+        time.sleep(2)
+        driver.execute_script('document.getElementById("generate").click()')
+        time.sleep(2)
         try:
-            driver.find_element(By.XPATH, '/html/body/div[6]').click()
+            driver.execute_script("document.querySelector('.btn.btn-primary.btn-block').click();")
         except:
-            driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[4]/button').click()
-        try:
+            while "Too many requests, please try again later." in driver.page_source:
+                time.sleep(10)
+                driver.get(domain)
+        time.sleep(2)
+    
+        if "https://bstlar.com" in driver.current_url:
+            for i in range(3):
+                try:
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[0])
+                    driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/button').click()
+                    link1 = driver.page_source
+                    link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
+                    link1 = link1[1].split('" target="_BLANK')
+                    link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
+                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[1])
+                    driver.close()
+                except:pass
+            for i in range(3):
+                try:
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[0])
+                    driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[2]/button').click()
+                    link1 = driver.page_source
+                    link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
+                    link1 = link1[1].split('" target="_BLANK')
+                    link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
+                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[1])
+                    driver.close()
+                except:pass
+            for i in range(3):
+                try:
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[0])
+                    driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[3]/button').click()
+                    link1 = driver.page_source
+                    link1 = link1.split('https://searchwithme.net/redirect?tid=')
+                    link1 = link1[1].split('" target="_BLANK')
+                    link1 = f"https://searchwithme.net/redirect?tid={link1[0]}"
+                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
+                    tabs = driver.window_handles
+                    driver.switch_to.window(tabs[1])
+                    driver.close()
+                except:pass
+    
             tabs = driver.window_handles
-            driver.switch_to.window(tabs[1])
-            driver.close()
             driver.switch_to.window(tabs[0])
-        except:pass
-    links += 1
-
+            time.sleep(10)
+            while not "Unlock progress: 3 /" in driver.page_source:
+                time.sleep(0.1)
+            try:
+                driver.find_element(By.XPATH, '/html/body/div[6]').click()
+            except:
+                driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[4]/button').click()
+            try:
+                tabs = driver.window_handles
+                driver.switch_to.window(tabs[1])
+                driver.close()
+                driver.switch_to.window(tabs[0])
+            except:pass
+        links += 1
+    except:
+        main()
 while True:
     main()
