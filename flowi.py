4008d1426f60222d98455cbbbce42f14d35979b7
baum1810 <72774679+baum1810@users.noreply.github.com>
2023-03-03 11:21:30 +0100

    Update flowi.py

--- flowi.py
+++ flowi.py
@@ -4,7 +4,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 
 #add your cookie here
-cookie = "yourcookie"
+cookie = "YOURCOOKIE"
 #domain of flow here they sometimes change the domain
 domain = "https://flownew.vercel.app/"
 
@@ -15,91 +15,54 @@ domain = "https://flownew.vercel.app/"
 
 # dont touch the shit under this line if u dont know what you are doing
 options = uc.ChromeOptions()
-driver = uc.Chrome()
+options.add_argument("--disable-popup-blocking")
+driver = uc.Chrome(options=options)
 driver.get(domain)
 driver.add_cookie({'name' : 'COOKIE', 'value' : cookie})
 driver.get(domain)
 links = 0
 
 def main():
-    try:
-        driver.get(domain)
-        global  links
-        system("cls")
-        print(f"Links: {links}")
-        time.sleep(2)
-        driver.execute_script('document.getElementById("generate").click()')
-        time.sleep(2)
         try:
-            driver.execute_script("document.querySelector('.btn.btn-primary.btn-block').click();")
-        except:
-            while "Too many requests, please try again later." in driver.page_source:
-                time.sleep(10)
-                driver.get(domain)
-        time.sleep(2)
-        if "https://bstlar.com/error-404" in driver.current_url:
-               main() 
-        if "https://bstlar.com" in driver.current_url:
-            for i in range(3):
-                try:
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[0])
-                    driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/button').click()
-                    link1 = driver.page_source
-                    link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
-                    link1 = link1[1].split('" target="_BLANK')
-                    link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
-                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[1])
-                    driver.close()
-                except:pass
-            for i in range(3):
-                try:
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[0])
-                    driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[2]/button').click()
-                    link1 = driver.page_source
-                    link1 = link1.split('https://entlyhaveb.autos/redirect?tid=')
-                    link1 = link1[1].split('" target="_BLANK')
-                    link1 = f"https://entlyhaveb.autos/redirect?tid={link1[0]}"
-                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[1])
-                    driver.close()
-                except:pass
-            for i in range(3):
-                try:
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[0])
-                    driver.find_element(By.XPATH,'//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[2]/div[3]/button').click()
-                    link1 = driver.page_source
-                    link1 = link1.split('https://searchwithme.net/redirect?tid=')
-                    link1 = link1[1].split('" target="_BLANK')
-                    link1 = f"https://searchwithme.net/redirect?tid={link1[0]}"
-                    driver.execute_script('''document.querySelector('a[href="'''+link1+'''"]').click();''')
-                    tabs = driver.window_handles
-                    driver.switch_to.window(tabs[1])
-                    driver.close()
-                except:pass
-
+            
+            global  links
+            global domain
             tabs = driver.window_handles
             driver.switch_to.window(tabs[0])
-            time.sleep(10)
-            while not "Unlock progress: 3 /" in driver.page_source:
-                time.sleep(0.1)
+            driver.get(domain)
+    
+            system("cls")
+            print(f"Links: {links}")
+            time.sleep(2)
+            driver.execute_script('document.getElementById("generate").click()')
+            time.sleep(2)
+    
+    
+            while "Too many requests, please try again later." in driver.page_source:
+                    time.sleep(10)
+                    driver.get(domain)
+            time.sleep(5)
             try:
-                driver.find_element(By.XPATH, '/html/body/div[6]').click()
+                driver.find_element(By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
+                
             except:
-                driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/div/div/div/div[4]/button').click()
+                try:
+                    driver.execute_script("document.querySelector('#qc-cmp2-ui > div:nth-child(2) > div > button:nth-child(2)').click();")
+                except:pass
             try:
-                tabs = driver.window_handles
-                driver.switch_to.window(tabs[1])
-                driver.close()
-                driver.switch_to.window(tabs[0])
-            except:pass
-        links += 1
-    except:
-        main()
+                driver.execute_script("""document.evaluate('/html/body/div[2]/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[2]/button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();""")
+            except:
+                driver.execute_script("document.getElementById('destination-button').click();")
+            tabs = driver.window_handles
+            driver.switch_to.window(tabs[1])
+            time.sleep(8)
+            driver.execute_script("location.replace('https://flownew.vercel.app/reward?key=' + document.getElementById('tokenfield').value)")
+            driver.switch_to.window(tabs[0])
+            time.sleep(1)
+            driver.close()
+            links +=1
+        except:
+           
+            main()
 while True:
     main()
